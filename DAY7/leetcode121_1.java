b8a97d0f17d06dc3a44adfee78d701557353ec0d
Pranshu Raj <rajpranshu2222@gmail.com>
2023-12-28 11:08:29 +0530

    Update leetcode121_1.java

--- DAY7/leetcode121_1.java
+++
@@ -1,9 +0,0 @@
-int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
-
-        for (int price : prices) {
-            minPrice = Math.min(minPrice, price);
-            maxProfit = Math.max(maxProfit, price - minPrice);
-        }
-
-        return maxProfit;
